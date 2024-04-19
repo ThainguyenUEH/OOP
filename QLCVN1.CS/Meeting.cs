5d2fe39ab877e55c6d4c83304b386a140ab02ba6
ThaiNguyenUEH <thainguyen31221023457@st.ueh.edu.vn>
2024-04-19 20:04:17 +0700

    first commit

---
+++ QLCVN1.CS/Meeting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace QLCVN1.CS
+{
+    public class Meeting
+    {
+        private string project;
+        private DateTime _date;
+        private string _topic;
+        private string _content;
+
+        // Encapsulation: Đảm bảo tính đóng gói của các thuộc tính
+        public string Project
+        {
+            get { return project; }
+            set { project = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public string Topic
+        {
+            get { return _topic; }
+            set { _topic = value; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        // Constructor để khởi tạo một đối tượng Meeting
+        public Meeting(string project, DateTime date, string topic, string content)
+        {
+            Project = project;
+            Date = date;
+            Topic = topic;
+            Content = content;
+        }
+
+        // Abstraction: Hiện thực một phương thức trừu tượng để hiển thị thông tin về cuộc họp
+        public virtual void DisplayMeetingInfo()
+        {
+            Console.WriteLine($"Project: {Project}");
+            Console.WriteLine($"Date: {Date.ToShortDateString()}");
+            Console.WriteLine($"Topic: {Topic}");
+            Console.WriteLine($"Content: {Content}");
+        }
+    }
+}
---
+++ QLCVN1.CS/Member.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QLCVN1.CS
+{
+    // Inheritance: kế thừa các thuộc tính và phương thức từ lớp cơ sở
+    public class Member : Employee
+    {
+        public Member(string id, string name, string email, bool gender, int age, Project project)
+            : base(id, name, email, gender, age)
+        {
+          //  Projects = new List<Project>();
+           // Projects.Add(project);
+        }
+     //   public List<Project> Projects { get; set; }
+
+        // Polymorphism: cùng một phương thức hoạt động khác nhau ở các lớp con
+        public override string ToString()
+        {
+            return $"ID: {Id}\n" +
+                   $"Tên: {Name}\n" +
+                   $"Email: {Email}\n" +
+                   $"Giới tính: {(Gender ? "Nam" : "Nữ")}\n" +
+                   $"Tuổi: {Age}\n";
+        }
+    }
+}
